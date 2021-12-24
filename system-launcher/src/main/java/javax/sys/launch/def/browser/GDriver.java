5407efcb3e7a06d969af7d0f925cd9d24bdb5a01
TW-Develop <82227609+TW-Develop@users.noreply.github.com>
2021-12-24 18:51:49 +0100

    Improvement of the class design. Creation of a class description.

--- system-launcher/src/main/java/javax/sys/launch/def/browser/GDriver.java
+++ system-launcher/src/main/java/javax/sys/launch/def/browser/GDriver.java
@@ -19,24 +19,36 @@ import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.openqa.selenium.WebDriver;
-
+/*personally classes*/
+import javax.sys.launch.def.browser.plattform.DriverInstance;
 import javax.sys.launch.def.browser.plattform.Sniffer;
 import java.lang.reflect.InvocationTargetException;
-
 /**
- *
+ * System driver, which automates and system-specific performs
+ * the specified Selenium Driver, do without preparations to meet.
  */
-public @NotNull record GDriver(String browserName) {
-
+public @NotNull record GDriver(DriverInstance instance) {
+    /**
+     * Analyzes the OS and its default web browser to play this as a stable class.
+     * @return  standard selenium driver, which has been
+     *          stored in the system settings of the OS
+     */
     public static @Nullable WebDriver systemExplorer() {
-        try {return new GDriver(Sniffer.systemBrowserName()).createDriverInstance();}
-        catch (Exception e) {return null;}
+        try {
+            return new GDriver(Sniffer.systemBrowser()).createDriverInstance();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
+    /**
+     * creates a web-driver instance of the default browser of the current OS
+     * @return a web-driver instance of the system default driver
+     */
     @NotNull WebDriver createDriverInstance() throws ClassNotFoundException, NoSuchMethodException,
             InvocationTargetException, InstantiationException, IllegalAccessException {
         //create protocol handshake
-        WebDriverManager manage = WebDriverManager.getInstance(DriverManagerType.valueOf(browserName));
+        WebDriverManager manage = WebDriverManager.getInstance(DriverManagerType.valueOf(instance.name()));
         manage.setup();
         //create driver instance
         return (WebDriver) ((java.lang.reflect.Constructor<?>)
---
+++ system-launcher/src/main/java/javax/sys/launch/def/browser/plattform/DriverInstance.java
@@ -0,0 +1,30 @@
+package javax.sys.launch.def.browser.plattform;
+
+/**
+ *
+ */
+public enum DriverInstance {
+
+    CHROME ("ChromeHTML","chrome.exe"),
+    @Deprecated EDGE("AppXq0fevzme2pys62n3e0fbqa7peapykr8v","msedge.exe"),
+    MSEDGE("MSEdgeHTM","msedge.exe"),
+    FIREFOX("FirefoxURL-308046B0AF4A39CB",""),
+    IEXPLORER ("IE.HTTP","iexplore.exe");
+
+    private final JBrowser browser;
+
+    DriverInstance(String rgx, String prc) {
+        this.browser = new JBrowser(rgx, prc);
+    }
+
+    public @org.jetbrains.annotations.NotNull JBrowser get() {
+        return this.browser;
+    }
+
+    /**
+     * Template class for System-driver-constant
+     */
+    record JBrowser(String regex, String processCall) {
+        //None content...
+    }
+}
