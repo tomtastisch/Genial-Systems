f315a076a981eecb2baed0cdb5a9540f70593c89
TW-Develop <82227609+TW-Develop@users.noreply.github.com>
2021-12-26 17:19:28 +0100

    Initial commit

--- system-launcher/src/test/java/javax.sys.launch.def.browser/GDriverTest.java
+++ system-launcher/src/test/java/javax.sys.launch.def.browser/GDriverTest.java
@@ -1,14 +1,19 @@
 package javax.sys.launch.def.browser;
 
+import net.jodah.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 
 import javax.sys.launch.def.browser.plattform.SystemExplorer;
+import java.util.Objects;
 
 public class GDriverTest {
 
     @Test public void build_driver() {
-        try (SystemExplorer wdi = GDriverFactory.systemExplorer()) {
-            wdi.createDriverInstance();
+        try (SystemExplorer explorer = GDriverFactory.systemExplorer()) {
+            WebDriver driver = explorer.createDriverInstance();
+
+            assert Objects.nonNull(driver);
         } catch(Exception e) {
             e.printStackTrace();
         }
