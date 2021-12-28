5632edb0748cecc9df9fa55e9e845c0d0ac7d2e3
TW-Develop <82227609+TW-Develop@users.noreply.github.com>
2021-12-28 14:04:41 +0100

    Kleiner Fehlerbehebungen

--- system-launcher/src/main/java/javax/sys/launch/def/browser/plattform/ExplorerValueMapper.java
+++ system-launcher/src/main/java/javax/sys/launch/def/browser/plattform/ExplorerValueMapper.java
@@ -12,16 +12,6 @@ public interface ExplorerValueMapper extends AutoCloseable {
     Multimap<Long, Object> queue = ArrayListMultimap.create();
     /** @return class id */
     long id();
-
-    /**
-     * The call {@link AutoCloseable#close() <class>.close()} is effectively equivalent to the call:<br>
-     * <pre>    {@link ExplorerValueMapper#clean(Object, Runnable) <class>.clean(o, r)}</pre>
-     * in automated form with own error handling.
-     *
-     * @throws Exception for handling errors
-     */
-    @Override void close() throws Exception;
-
     /**
      * Cleaning method for destroying all created and no longer required variables
      * @param o cleanable object
